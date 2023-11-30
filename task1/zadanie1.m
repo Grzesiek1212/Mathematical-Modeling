a473c2dd8bb0f7de9f39b084840e3d326615c7a7
Grzegorz Prasek <127451174+Grzesiek1212@users.noreply.github.com>
2023-11-30 01:37:37 +0100

    Delete task1 directory

--- task1/zadanie1.m
+++
@@ -1,3 +0,0 @@
-function zadanie1(N)
-    X = 1:N;
-    fplot(X, zadanie1a(2,2,X),X, zadanie1b(2,2,X),X, zadanie1c(2,2,X))
--- task1/zadanie1b.m
+++
@@ -1,8 +0,0 @@
-function zadanie1b(a,r,N)
-% 2 wariant
-    tic
-    B = ones(1,N)*a;
-    D = ones(1,N)*r;
-    B = B.*(D.^(0:N-1));
-    %disp(B);
-    toc
